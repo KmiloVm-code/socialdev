1a6b4e5d569bc8f73e784b1d5a8167438250c423
KmiloVm <kmilo7v@gmail.com>
2025-10-14 23:13:01 -0500

    feat: Add chat item component with styles and integrate search bar in chat module

--- frontend/src/app/features/chat/chat-module.ts
+++ frontend/src/app/features/chat/chat-module.ts
@@ -5,13 +5,15 @@ import { ChatRoutingModule } from './chat-routing-module';
 import { Chat } from './chat';
 import { ChatList } from './components/chat-list/chat-list';
 import { ChatRoom } from './components/chat-room/chat-room';
+import { ChatItem } from './components/chat-item/chat-item';
 
 
 @NgModule({
   declarations: [
     Chat,
     ChatList,
-    ChatRoom
+    ChatRoom,
+    ChatItem
   ],
   imports: [
     CommonModule,
---
+++ frontend/src/app/features/chat/components/chat-item/chat-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-item',
+  standalone: false,
+  templateUrl: './chat-item.html',
+  styleUrl: './chat-item.css'
+})
+export class ChatItem {
+
+}
