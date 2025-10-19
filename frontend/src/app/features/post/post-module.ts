2ceba4a379cb7243442d4cddfc70dc305a57bbdf
KmiloVm <kmilo7v@gmail.com>
2025-10-18 22:21:34 -0500

    feat: Refactor post and profile modules to include SharedModule; update button components in user info and post forms

--- frontend/src/app/features/post/post-module.ts
+++ frontend/src/app/features/post/post-module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared-module';
 
 import { PostRoutingModule } from './post-routing-module';
 import { Post } from './post';
 
-
 @NgModule({
-  declarations: [
-    Post
-  ],
-  imports: [
-    CommonModule,
-    PostRoutingModule
-  ]
+  declarations: [Post],
+  imports: [CommonModule, PostRoutingModule, SharedModule],
 })
-export class PostModule { }
+export class PostModule {}
--- frontend/src/app/features/profile/profile-module.ts
+++ frontend/src/app/features/profile/profile-module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared-module';
 
 import { ProfileRoutingModule } from './profile-routing-module';
 import { Profile } from './profile';
 import { UserInfo } from './components/user-info/user-info';
 import { UserProjects } from './components/user-projects/user-projects';
 
-
 @NgModule({
-  declarations: [
-    Profile,
-    UserInfo,
-    UserProjects
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [Profile, UserInfo, UserProjects],
+  imports: [CommonModule, ProfileRoutingModule, SharedModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
